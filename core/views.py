8b2b6562965a9bbb8ae88f47c68fe74ccc467069
Hoàng Sơn <hoangson@192.168.2.126>
2021-02-23 12:17:24 +0700

    Convert Function base views to class base views

--- core/views.py
+++ core/views.py
@@ -1,23 +1,49 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def test_view(request):
-    data = {
-        'name': 'Son',
-        'age': 27,
-    }
-    return JsonResponse(data)
 
-def test_view2(request):
-    data = [
-        {
+class TestView(APIView):
+    
+    def get(self, request):
+        data = {
             'name': 'Son',
             'age': 27,
-        },
-        {
-            'name': 'Dat',
-            'age': 27,
-        },
-    ]
-    return JsonResponse(data, safe=False)
+        }
+        return Response(data)
+
+class TestView2(APIView):
+
+    def get(self, request):
+        data = [
+            {
+                'name': 'Son',
+                'age': 27,
+            },
+            {
+               'name': 'Dat',
+               'age': 27,
+            },
+        ]
+        return Response(data)
+
+# def test_view(request):
+#     data = {
+#         'name': 'Son',
+#         'age': 27,
+#     }
+#     return JsonResponse(data)
+
+# def test_view2(request):
+#     data = [
+#         {
+#             'name': 'Son',
+#             'age': 27,
+#         },
+#         {
+#             'name': 'Dat',
+#             'age': 27,
+#         },
+#     ]
+#     return JsonResponse(data, safe=False)
--- demo_api/urls.py
+++ demo_api/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from core.views import test_view, test_view2
+from core.views import TestView, TestView2
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('test1/', test_view),
-    path('test2/', test_view2),
+    path('test1/', TestView.as_view()),
+    path('test2/', TestView2.as_view()),
 ]
