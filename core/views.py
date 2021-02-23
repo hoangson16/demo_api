60fc9a11090cc36cc3a3b95e51e5ffc27ece29ad
Hoàng Sơn <hoangson@192.168.2.126>
2021-02-23 11:43:49 +0700

    Add some test views api.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+def test_view(request):
+    data = {
+        'name': 'Son',
+        'age': 27,
+    }
+    return JsonResponse(data)
+
+def test_view2(request):
+    data = [
+        {
+            'name': 'Son',
+            'age': 27,
+        },
+        {
+            'name': 'Dat',
+            'age': 27,
+        },
+    ]
+    return JsonResponse(data, safe=False)
